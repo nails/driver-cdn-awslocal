79218fd7340456abe0db6f6c83d4a1300f371a11
Pablo de la Peña <p@shedcollective.org>
2021-03-26 09:44:07 +0000

    Add getPermissions() to Settings

--- src/Aws/Settings/Aws.php
+++ src/Aws/Settings/Aws.php
@@ -38,6 +38,16 @@ class Aws implements Interfaces\Component\Settings
     /**
      * @inheritDoc
      */
+    public function getPermissions(): array
+    {
+        return [];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
     public function get(): array
     {
         /** @var Setting $oAccessKey */
