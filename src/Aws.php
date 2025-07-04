ea7252fc56b78cbb0d6dd22b7e7fdf7e8368aebc
Pablo de la Peña <p@shedcollective.org>
2025-07-04 15:02:08 +0100

    chore: Fix exception

--- src/Aws.php
+++ src/Aws.php
@@ -217,14 +217,14 @@ class Aws extends Local
 
     public function objectMove($sObject, $sBucket)
     {
-        throw new DriverException('The AWS CDN driver does not support moving objects.');
+        throw new \Exception('The AWS CDN driver does not support moving objects.');
     }
 
     // --------------------------------------------------------------------------
 
     public function objectCopy($sObject, $sBucket)
     {
-        throw new DriverException('The AWS CDN driver does not support copying objects.');
+        throw new \Exception('The AWS CDN driver does not support copying objects.');
     }
 
     // --------------------------------------------------------------------------
